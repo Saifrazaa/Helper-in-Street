26030971ceecf329cbc5793d6c304be05d9cfca0
saif raza <razasaif171@gmail.com>
2017-12-06 23:29:13 +0500

    template setting

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'About Us' });
+});
+router.get('/registration', function(req, res, next) {
+  res.render('registration', { title: 'Contact Us' });
+});
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Portfolio' });
+});
+router.get('/aboutus', function(req, res, next) {
+  res.render('about', { title: 'About Us' });
+});
+router.get('/contactus', function(req, res, next) {
+  res.render('contact', { title: 'Contact Us' });
+});
+
+module.exports = router;
